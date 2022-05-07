5860fce1b539d2dfed23cbb768b4ccd4424c99ec
KongBoy <s89334roy@yahoo.com.tw>
2022-05-07 23:16:23 +0800

    step2 的 analyze 改成 也可以看 F 的 min/max

--- step2_analyze_wc_xyz_min_max.py
+++ step2_analyze_wc_xyz_min_max.py
@@ -27,7 +27,7 @@ import os
 import time
 import matplotlib.pyplot as plt
 
-def wc_xyz_min_max(wc_paths):
+def F_ow_W_ch_min_max(data_paths, task_amount, core_amount=10, print_msg=False):
     start_time = time.time()
     with Manager() as manager:  ### 設定在 multiprocess 裡面 共用的 list
         ### 以下想 multiprocess
@@ -38,9 +38,7 @@ def wc_xyz_min_max(wc_paths):
         ch1_mins = manager.list()  # []的概念
         ch2_maxs = manager.list()  # []的概念
         ch2_mins = manager.list()  # []的概念
-        core_amount = wc_amount // 150
-        task_amount = wc_amount
-        _wx_xyz_min_max_multiprocess(wc_paths, ch0_maxs, ch0_mins, ch1_maxs, ch1_mins, ch2_maxs, ch2_mins, core_amount=core_amount, task_amount=task_amount)
+        _F_ow_W_ch_min_max_multiprocess(data_paths, ch0_maxs, ch0_mins, ch1_maxs, ch1_mins, ch2_maxs, ch2_mins, core_amount=core_amount, task_amount=task_amount, print_msg=print_msg)
 
         ### finish ~~ 開始show 結果囉
         ### 先整個list秀出來 大略看看 每個切出來的小task找的狀況
@@ -70,47 +68,81 @@ def wc_xyz_min_max(wc_paths):
 
     print("total_cost_time:", time.time() - start_time)
     """
-    ch0_maxs.max() 0.63452387
-    ch0_mins.min() -0.67187124
-    ch1_maxs.max() 1.2387834
-    ch1_mins.min() -1.2280148
-    ch2_maxs.max() 1.2485291
-    ch2_mins.min() -1.2410645
-    total_cost_time: 4970.9831392765045
-    total_cost_time: 4822.389922380447
+    doc3d
+        uv
+            M ch0_maxs.max() 1.0
+            M ch0_mins.min() 0.0
+            y ch1_maxs.max() 1.0
+            y ch1_mins.min() 0.0
+            x ch2_maxs.max() 1.0000006
+            x ch2_mins.min() 0.0
+            total_cost_time: 4322.7983639240265
 
-    ### DewarpNet裡寫的數值： 1.2539363, -1.2442188, 1.2396319, -1.2289206, 0.6436657, -0.67492497 # RGB -> BGR
+        wc
+            z ch0_maxs.max()  0.63452387
+            z ch0_mins.min() -0.67187124
+            x ch1_maxs.max()  1.2387834
+            x ch1_mins.min() -1.2280148
+            y ch2_maxs.max()  1.2485291
+            y ch2_mins.min() -1.2410645
+            total_cost_time: 4970.9831392765045
+            total_cost_time: 4822.389922380447
+            total_cost_time: 5963.885419368744  ### 2022/05/04
+
+            ### DewarpNet裡寫的數值： 1.2539363, -1.2442188, 1.2396319, -1.2289206, 0.6436657, -0.67492497 # RGB -> BGR
+
+    kong_doc3d(驗算一下， 順便看 kong_doc3d 有沒有寫對)
+        uv
+            ch0_maxs.max() 1.0
+            ch0_mins.min() 0.0
+            ch1_maxs.max() 1.0
+            ch1_mins.min() 0.0
+            ch2_maxs.max() 1.0000006
+            ch2_mins.min() 0.0
+            total_cost_time: 7074.6238849163055
+        wc
+            # ch0_maxs.max() 0.63452387
+            # ch0_mins.min() -0.67187124
+            # ch1_maxs.max() 1.2387834
+            # ch1_mins.min() -1.2280148
+            # ch2_maxs.max() 1.2485291
+            # ch2_mins.min() -1.2410645
+            # total_cost_time: 5963.885419368744
     """
 
-def _wx_xyz_min_max_multiprocess(wc_paths, ch0_maxs, ch0_mins, ch1_maxs, ch1_mins, ch2_maxs, ch2_mins, core_amount, task_amount):
-    multi_processing_interface(core_amount=core_amount, task_amount=task_amount, task=_wc_xyz_min_max, task_args=[wc_paths, ch0_maxs, ch0_mins, ch1_maxs, ch1_mins, ch2_maxs, ch2_mins], print_msg=True)
+def _F_ow_W_ch_min_max_multiprocess(data_paths, ch0_maxs, ch0_mins, ch1_maxs, ch1_mins, ch2_maxs, ch2_mins, core_amount, task_amount, print_msg=False):
+    multi_processing_interface(core_amount=core_amount, task_amount=task_amount, task=_F_ow_W_ch_min_max, task_args=[data_paths, ch0_maxs, ch0_mins, ch1_maxs, ch1_mins, ch2_maxs, ch2_mins], print_msg=print_msg)
 
-def _wc_xyz_min_max(start_index, amount, wc_paths, ch0_maxs, ch0_mins, ch1_maxs, ch1_mins, ch2_maxs, ch2_mins):
+def _F_ow_W_ch_min_max(start_index, amount, data_paths, ch0_maxs, ch0_mins, ch1_maxs, ch1_mins, ch2_maxs, ch2_mins):
     '''
     主要做事的地方在這裡喔！
     '''
-    wcs = []
+    datas = []
     # start_index = 0
     # amount = 100
-    for i, file_path in enumerate(tqdm(wc_paths[start_index:start_index + amount])):
+    for i, file_path in enumerate(tqdm(data_paths[start_index:start_index + amount])):
         # print(i, file_path)
-        wcs.append(cv2.imread(file_path, cv2.IMREAD_ANYDEPTH | cv2.IMREAD_UNCHANGED))  ### 這行就可以了！
+        # print(i, file_path[-4:])
+        if  (".exr" in file_path[-4:].lower()): datas.append(cv2.imread(file_path, cv2.IMREAD_ANYDEPTH | cv2.IMREAD_UNCHANGED))
+        elif(".npy" in file_path[-4:].lower()): datas.append(np.load(file_path))
+        elif(".jpg" in file_path[-4:].lower() or
+             ".png" in file_path[-4:].lower()): datas.append(cv2.imread(file_path))
 
-    wcs  = np.array(wcs)  ### list 轉 numpy
-    # print(wcs.shape)
-    # print("ch0 max:", wcs[..., 0].max())
-    # print("ch0 min:", wcs[..., 0].min())
-    # print("ch1 max:", wcs[..., 1].max())
-    # print("ch1 min:", wcs[..., 1].min())
-    # print("ch2 max:", wcs[..., 2].max())
-    # print("ch2 min:", wcs[..., 2].min())
+    datas  = np.array(datas)  ### list 轉 numpy
+    # print(datas.shape)
+    # print("ch0 max:", datas[..., 0].max())
+    # print("ch0 min:", datas[..., 0].min())
+    # print("ch1 max:", datas[..., 1].max())
+    # print("ch1 min:", datas[..., 1].min())
+    # print("ch2 max:", datas[..., 2].max())
+    # print("ch2 min:", datas[..., 2].min())
     # print()
-    ch0_maxs.append( wcs[..., 0].max())
-    ch0_mins.append( wcs[..., 0].min())
-    ch1_maxs.append( wcs[..., 1].max())
-    ch1_mins.append( wcs[..., 1].min())
-    ch2_maxs.append( wcs[..., 2].max())
-    ch2_mins.append( wcs[..., 2].min())
+    ch0_maxs.append( datas[..., 0].max())
+    ch0_mins.append( datas[..., 0].min())
+    ch1_maxs.append( datas[..., 1].max())
+    ch1_mins.append( datas[..., 1].min())
+    ch2_maxs.append( datas[..., 2].max())
+    ch2_mins.append( datas[..., 2].min())
 ############################################################################################################################################
 ############################################################################################################################################
 ############################################################################################################################################
@@ -130,8 +162,8 @@ def wc_2D_3D_and_uv_visual(wc_paths, uv_paths, page_names_w_dir, dst_dir):
         Check_dir_exist_and_build_new_dir(uv_2d_dst_dir + "/%i" % dir_name)
 
 
-    core_amount = wc_amount // 70
-    task_amount = wc_amount
+    core_amount = datas_amount // 70
+    task_amount = datas_amount
     _wc_2D_3D_and_uv_visual_multiprocess(wc_paths, uv_paths, page_names_w_dir, wc_2d_dst_dir, wc_3d_dst_dir, uv_2d_dst_dir, core_amount=core_amount, task_amount=task_amount)
 
 
@@ -154,7 +186,7 @@ def _wc_2D_3D_and_uv_visual(start_index, amount, wc_paths, uv_paths, page_names_
         wc_3D_good_to_v = wc[..., ::-1]  ### 嘗試幾次後，這樣子比較好看
 
         wc_3d_dst_path = wc_3d_dst_dir + "/" + page_names_w_dir[i] + ".png"
-        fig, ax = wc_3d_plot(wc_3D_good_to_v, mask, fewer_point=True, small_size=(300, 300), figsize=(5, 5))
+        fig, ax = wc_3d_plot(wc_3D_good_to_v, mask, fewer_point=True, small_size=(300, 300), ax_size=5)
         plt.savefig(wc_3d_dst_path)
 
         wc_2d_dst_path = wc_2d_dst_dir + "/" + page_names_w_dir[i] + ".png"
@@ -171,7 +203,8 @@ def _wc_2D_3D_and_uv_visual(start_index, amount, wc_paths, uv_paths, page_names_
 ############################################################################################################################################
 
 if(__name__ == "__main__"):
-    from step0_Doc3D_obj import  using_doc3D
+    from step0_Doc3D_obj  import using_doc3D
+    from step0_Kong_Doc3D import kong_doc3D
     '''1_244_1-cp_Page_0995-mpT0001.exr'''
     ### 取得 wc_paths
     wc_paths = using_doc3D.wc_paths
@@ -180,13 +213,16 @@ if(__name__ == "__main__"):
     # for wc_path in wc_paths: print(wc_path)
 
     ### 設定 要處理的數量
-    # wc_amount = 2000 #len(wc_paths)  ### 少量測試時用的
-    wc_amount = len(wc_paths)
-    # print("wc_amount:", wc_amount)
+    # datas_amount = 2000 #len(wc_paths)  ### 少量測試時用的
+    datas_amount = len(wc_paths)
+    # print("datas_amount:", datas_amount)
     ############################################################################################################
 
     ### 分析1：找 整個DB 所有 wc 各個channel 的 min/max
-    # wc_xyz_min_max(wc_paths)
+    # F_ow_W_ch_min_max(wc_paths, datas_amount, core_amount = datas_amount // 150)  ### core_amount 這樣設可避免 爆記憶體 喔
+    # F_ow_W_ch_min_max(uv_paths, datas_amount, core_amount = datas_amount // 150)  ### core_amount 這樣設可避免 爆記憶體 喔
+    # F_ow_W_ch_min_max(kong_doc3D.uv_npy_paths   , datas_amount, core_amount=datas_amount // 150)  ### core_amount 這樣設可避免 爆記憶體 喔
+    F_ow_W_ch_min_max(kong_doc3D.W_w_M_npy_paths, datas_amount, core_amount=datas_amount // 150)  ### core_amount 這樣設可避免 爆記憶體 喔
 
     ### 分析2
     # wc_2D_3D_and_uv_visual(wc_paths, uv_paths, page_names_w_dir, dst_dir=r"H:")
