7af3de135e762f4ffe259ce1f9399a48930a2c5a
KongBoy <s89334roy@yahoo.com.tw>
2022-05-12 15:36:59 +0800

    更新 step1_b_get_dtd_name 讓 127.27 可用 GPU train DewarpNet這樣子

--- step1_b_get_dtd_name.py
+++ step1_b_get_dtd_name.py
@@ -2,16 +2,17 @@
 augtexnames.txt 這應該是給 DewarpNet 用的喔～
 '''
 import os
-from step0_Doc3D_obj import doc3D
+from step0_Doc3D_obj import real_doc3D
 
-aug_path = doc3D.db_root[: len(doc3D.db_root.split("/")[-1]) * -1 - 1] + "/" + "augtexnames.txt"
+aug_path = real_doc3D.db_root[: len(real_doc3D.db_root.split("/")[-1]) * -1 - 1] + "/" + "augtexnames.txt"
 if os.path.isfile(aug_path): os.remove(aug_path)
 
-dtd_img_root = "dtd/images"
-dtd_img_dirs = os.listdir(dtd_img_root)
+dtd_img_root_read_dir = r"J:\dtd\images"
+dtd_img_root_write_str = "/dtd/images"  ### 最前面一訂要加 / 喔！ 為了 配合 人家寫的code這樣子拉
+dtd_img_dirs = os.listdir(dtd_img_root_read_dir)
 names = []
 for img_dir in dtd_img_dirs:
-    names += [dtd_img_root + "/" + img_dir + "/" + name for name in os.listdir(dtd_img_root + "/" + img_dir)]
+    names += [dtd_img_root_write_str + "/" + img_dir + "/" + name for name in os.listdir(dtd_img_root_read_dir + "/" + img_dir)]
 
 with open(aug_path, "w") as f:
     for name in names:
